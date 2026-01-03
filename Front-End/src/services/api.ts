46787e99f2b040b11f261693b6a4a90df5e9b802
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-01-03 16:06:54 -0300

    Deploy

--- Front-End/src/services/api.ts
+++ Front-End/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://kauanvinicius-onrender-com-wvfo.onrender.com",
 });
 
 // Cliente HTTP Axios
